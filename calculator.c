db93c95536471aeeb38285c1e19dfa5d3d7a3b55
05VSneha <v.sneha1@s.amity.edu>
2025-06-30 16:30:59 +0530

    subtraction

--- calculator.c
+++ calculator.c
@@ -18,3 +18,7 @@ int main() {
 			result= num1 + num2;
 			printf("Result: %.2lf\n", result);
 			break;
+				case '-':
+				result== num1 - num2;
+				printf("Result: %.2lf\n", result);
+				break;

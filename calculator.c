dc9bb1bc33315ae3e0e08d285310ce5099d1aaa3
05VSneha <v.sneha1@s.amity.edu>
2025-06-30 16:26:26 +0530

    addition

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+	double num1,num2;
+	char operator;
+	double result;
+
+	printf("Enter first number:");
+	scanf("%lf", &num1);
+        
+	printf("Enter an operator (+, -, *, /,):");
+	scanf("%c", &operator);
+
+	printf("Enter second  number:");
+        scanf("%lf", &num2);
+
+	switch(operator) {
+		case '+':
+			result= num1 + num2;
+			printf("Result: %.2lf\n", result);
+			break;
